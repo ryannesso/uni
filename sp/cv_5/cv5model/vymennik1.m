91bf55f2c5671571c6495e98ee60267b16088014
yehor kiriienko <ryantrololo10@gmail.com>
2026-03-17 21:29:34 +0100

    update: sp cv_5

---
+++ sp/cv_5/cv5model/vymennik1.m
@@ -0,0 +1,50 @@
+ro1 = 1000;
+ro2 = 770;
+cp1 = 4186;
+cp2 = 2420;
+M1 = 200;
+M2 = 300;
+T1i = 80;
+T2i = 18;
+Tok = 18;
+la12 = 1020000;
+la2ok = 21000;
+V1 = 0.0982;
+V2 = 0.0715;
+B1 = 0.9817;
+B2 = 1.4137;
+
+
+
+
+
+
+
+
+
+
+
+
+figure
+mesh(out.time),0:2:10,out.T20.Data,out.T21.Data, out.T22.Data,out.T23.Data,out.T24.Data,out.T25.Data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
